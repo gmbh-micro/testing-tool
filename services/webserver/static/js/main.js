055d39029347795d30cac788b1326e60006cc6ff
Abraham Dick <abedick@ku.edu>
2019-03-06 11:50:41 -0600

    added c0 test

--- services/webserver/static/js/main.js
+++ services/webserver/static/js/main.js
@@ -1,19 +1,14 @@
 "use strict";
 console.log("start");
+// @ts-ignore
 $(document).ready(function () {
-    // $('.collapsible').collapsible();
     setDashboard();
-    // retrieveData();
     buildNav();
 });
+var testSet = false;
 var container = document.getElementById("container");
 // let content = document.getElementById("main-content");  
 var nav = document.getElementById("nav-bar");
-// @ts-ignore
-var parsedServices;
-var parsedRemotes;
-function getAddress() {
-}
 function buildNav() {
     if (nav == null) {
         console.log("could not get nav bar");
@@ -21,7 +16,7 @@ function buildNav() {
     }
     nav.className += " nav-padding";
     nav.innerHTML =
-        "<span class=\"services-header\">navigation</span>\n<br>\n<div class=\"collection\">\n<a href=\"#!\" class=\"collection-item active white\"></a>\n<a href=\"#!\" class=\"collection-item\"></a>\n<a href=\"#!\" class=\"collection-item\"></a>\n<a href=\"#!\" class=\"collection-item\"></a>\n<a href=\"#!\" class=\"collection-item\"></a>\n</div>\n";
+        "<span class=\"services-header\">navigation</span>\n                    <br>\n                    <div class=\"collection\">\n                    <a href=\"#!\" class=\"collection-item active white\"></a>\n                    <a href=\"#!\" class=\"collection-item\"></a>\n                    <a href=\"#!\" class=\"collection-item\"></a>\n                    <a href=\"#!\" class=\"collection-item\"></a>\n                    <a href=\"#!\" class=\"collection-item\"></a>\n                    </div>\n                    ";
 }
 function setDashboard() {
     var content = document.getElementById("main-content");
@@ -29,6 +24,62 @@ function setDashboard() {
         console.log("error=could not get main-content div");
         return;
     }
-    content.innerHTML =
-        "<h3>Testing Tool</h3>\n";
+    content.innerHTML = "\n    <h3>Testing Tool</h3>\n    \n    <div class=\"input-field\">\n    <input id=\"input_text\" type=\"text\" data-length=\"10\">\n    <label for=\"input_text\">number</label>\n    </div>\n    <a class=\"waves-effect waves-light btn grey\" onclick=\"runTest();\">test</a>\n    ";
+}
+function runTest() {
+    console.log("running test");
+    var content = document.getElementById("main-content");
+    if (content == null) {
+        console.log("error=could not get main-content div");
+        return;
+    }
+    if (!testSet) {
+        content.appendChild(document.createElement('br'));
+        content.appendChild(document.createElement('br'));
+        var o = document.createElement('span');
+        o.innerHTML = "results";
+        content.appendChild(o);
+        content.appendChild(document.createElement('br'));
+        var ta = document.createElement("textarea");
+        ta.style.height = "unset";
+        ta.id = "output";
+        ta.setAttribute('rows', '20');
+        ta.setAttribute('cols', '84');
+        content.appendChild(ta);
+        content.appendChild(document.createElement('br'));
+        content.appendChild(document.createElement('br'));
+        testSet = true;
+    }
+    else {
+        var out = document.getElementById("output");
+        out.innerHTML = "";
+    }
+    var once = function (target, i, max) {
+        var out = document.getElementById("output");
+        out.innerHTML += "(" + (i + 1) + "/" + max + ") contacting c0\n";
+        $.ajax({
+            method: "GET",
+            url: target,
+        }).done(function (msg) {
+            var out = document.getElementById("output");
+            out.innerHTML += msg;
+        });
+        // var result = function(startTime) {
+        //     let out = document.getElementById("output");
+        //     out.innerHTML += msg;
+        // }
+        // var error = function(startTime) {
+        // }
+    };
+    var times = document.getElementById('input_text').value;
+    var max = 500;
+    try {
+        max = parseInt(times);
+    }
+    catch (_a) {
+        console.log("could not parse");
+    }
+    for (var i = 0; i < max; i++) {
+        setTimeout(once, i * 10, "c0", i, max);
+    }
 }
--- services/webserver/static/js/main.ts
+++ services/webserver/static/js/main.ts
@@ -2,24 +2,17 @@ console.log("start")
 
 // @ts-ignore
 $(document).ready(function(){
-    // $('.collapsible').collapsible();
     setDashboard();
-    // retrieveData();
     buildNav();
 });
 
+var testSet = false
+
 let container = document.getElementById("container");
 // let content = document.getElementById("main-content");  
-let nav = document.getElementById("nav-bar")
-
-// @ts-ignore
-let parsedServices;
-let parsedRemotes;
+let nav = document.getElementById("nav-bar");
 
 
-function getAddress(){
-
-}
 
 function buildNav(){
     if(nav == null){
@@ -29,20 +22,17 @@ function buildNav(){
     nav.className += " nav-padding";
 
     nav.innerHTML = 
-`<span class="services-header">navigation</span>
-<br>
-<div class="collection">
-<a href="#!" class="collection-item active white"></a>
-<a href="#!" class="collection-item"></a>
-<a href="#!" class="collection-item"></a>
-<a href="#!" class="collection-item"></a>
-<a href="#!" class="collection-item"></a>
-</div>
-`;
+                    `<span class="services-header">navigation</span>
+                    <br>
+                    <div class="collection">
+                    <a href="#!" class="collection-item active white"></a>
+                    <a href="#!" class="collection-item"></a>
+                    <a href="#!" class="collection-item"></a>
+                    <a href="#!" class="collection-item"></a>
+                    <a href="#!" class="collection-item"></a>
+                    </div>
+                    `;
 }
-
-
-
 
 function setDashboard(): void{
     
@@ -52,8 +42,80 @@ function setDashboard(): void{
         return;
     }  
 
-content.innerHTML =
-`<h3>Testing Tool</h3>
-`;
+    content.innerHTML = `
+    <h3>Testing Tool</h3>
+    
+    <div class="input-field">
+    <input id="input_text" type="text" data-length="10">
+    <label for="input_text">number</label>
+    </div>
+    <a class="waves-effect waves-light btn grey" onclick="runTest();">test</a>
+    `;
+}
 
+function runTest(){
+    console.log("running test");
+
+
+    let content = document.getElementById("main-content");
+    if (content == null) {
+        console.log("error=could not get main-content div")
+        return;
+    } 
+
+    if( !testSet){
+        content.appendChild(document.createElement('br'));
+        content.appendChild(document.createElement('br'));
+        let o = document.createElement('span');
+        o.innerHTML = "results";
+        content.appendChild(o);
+        content.appendChild(document.createElement('br'));
+    
+        let ta = document.createElement("textarea");
+        ta.style.height = "unset";
+        ta.id = "output";
+        ta.setAttribute('rows','20')
+        ta.setAttribute('cols','84')
+        content.appendChild(ta);
+        content.appendChild(document.createElement('br'));
+        content.appendChild(document.createElement('br'));
+
+        testSet = true;
+    } else {
+        let out = document.getElementById("output");
+        out.innerHTML = "";
+    }
+
+    var once = function(target: string, i: number,max: number){
+
+        let out = document.getElementById("output");
+        out.innerHTML += "(" + (i+1) + "/" + max +") contacting c0\n";
+        $.ajax({
+            method: "GET",
+            url: target,
+        }).done(function( msg ) {
+            let out = document.getElementById("output");
+            out.innerHTML += msg;
+        });
+
+        // var result = function(startTime) {
+        //     let out = document.getElementById("output");
+        //     out.innerHTML += msg;
+        // }
+        // var error = function(startTime) {
+        // }
+    }
+
+
+
+    let times = document.getElementById('input_text').value;
+    let max = 500;
+    try {
+        max = parseInt(times);
+    } catch {
+        console.log("could not parse");
+    }
+    for(let i = 0; i < max; i++){
+        setTimeout(once, i * 10, "c0",i,max);
+    }
 }
